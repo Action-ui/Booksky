2b6ef407f782bebdb32f18287c3f7366c240bd1d
Action-ui <kabilan2814@gmail.com>
2024-06-22 10:34:07 +0530

    Add files via upload

---
+++ BOOKSKY/index.js
@@ -0,0 +1,46 @@
+//selecting popoup box popup overlay overlay button
+var popupoverlay = document.querySelector('.popup-overlay')
+var popupbox = document.querySelector('.popup-box')
+var addpopupbutton = document.getElementById('add-popup-button')
+
+addpopupbutton.addEventListener('click',function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+})
+
+//select cancel button 
+
+var cancelpopup = document.getElementById('cancel-popup')
+cancelpopup.addEventListener('click',function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+}) 
+
+//select container ,add-book,book-title-input,book-author-input,book-description-input
+
+var container =  document.querySelector('.container')
+var addbook = document.getElementById('add-book')
+var booktitleinput = document.getElementById('book-title-input')
+var bookauthorinput = document.getElementById('book-author-input')
+var bookdescriptioninput = document.getElementById('book-description-input')
+
+addbook.addEventListener('click', function(event){
+      event.preventDefault()
+      var div = document.createElement('div')
+      div.setAttribute("class","book-container")
+      div.innerHTML=`<h2> ${booktitleinput.value}</h2>
+      <h5>${bookauthorinput.value}</h5>
+      <p> ${bookdescriptioninput.value}</P>
+      <button onclick="deletebutton(event)"> Delete </button> `
+      container.append(div)
+      popupoverlay.style.display="none"
+      popupbox.style.display="none"
+
+}) 
+
+function deletebutton(event){
+    event.target.parentElement.remove()
+
+}
+
